2de1161d03e06699629f5c286874a0761e057706
unknown <douglas.ferreirag@gmail.com>
2025-07-06 22:28:45 -0300

    Video1

---
+++ src/main/java/br/com/dio/model/BankService.java
@@ -0,0 +1,9 @@
+package br.com.dio.model;
+
+public enum BankService {
+
+    ACCOUNT,
+    INVESTMENT,
+
+
+}
---
+++ src/main/java/br/com/dio/model/InvestmentWallet.java
@@ -0,0 +1,31 @@
+package br.com.dio.model;
+
+import lombok.ToString;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+@ToString
+@Getter
+public class InvestmentWallet extends Wallet{
+
+    private final Investment investment;
+    private final AccountWallet account;
+
+    public InvestmentWallet(Investment investment, AccountWallet account, final long amount) {
+        super(INVESTMENT);
+        this.investment = investment;
+        this.account = account;
+        addMoney(account.reduceMoney(amount),getService(), "investimento");
+    }
+
+    public void updateAmount(final long percent){
+
+            var amount = getFunds() * percent / 100;
+            var history = new MoneyAudit(UUID.randomUUID(),getService(),"rendimentos", OffsetDateTime.now());
+            var money = Stream.generate(() -> new Money(history)).limit(amount).toList();
+            this.money.addAll(money);
+    }
+
+}
