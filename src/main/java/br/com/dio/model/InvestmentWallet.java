6c9c1ffffcdcb0f37cb64718da30d8abb3aa1870
unknown <douglas.ferreirag@gmail.com>
2025-07-08 19:49:25 -0300

    Criando exceptions e repositories

--- src/main/java/br/com/dio/model/InvestmentWallet.java
+++ src/main/java/br/com/dio/model/InvestmentWallet.java
@@ -2,6 +2,8 @@ package br.com.dio.model;
 
 import lombok.ToString;
 
+import lombok.Getter;
+
 import java.time.OffsetDateTime;
 import java.util.UUID;
 import java.util.stream.Stream;
@@ -9,23 +11,28 @@ import java.util.stream.Stream;
 @ToString
 @Getter
 public class InvestmentWallet extends Wallet{
-
     private final Investment investment;
     private final AccountWallet account;
 
-    public InvestmentWallet(Investment investment, AccountWallet account, final long amount) {
+    public InvestmentWallet(final Investment investment, final AccountWallet account, final long amount) {
         super(INVESTMENT);
         this.investment = investment;
         this.account = account;
-        addMoney(account.reduceMoney(amount),getService(), "investimento");
+        addMoney(account.reduceMoney(amount), getService(), "investmento");
     }
 
     public void updateAmount(final long percent){
-
-            var amount = getFunds() * percent / 100;
-            var history = new MoneyAudit(UUID.randomUUID(),getService(),"rendimentos", OffsetDateTime.now());
-            var money = Stream.generate(() -> new Money(history)).limit(amount).toList();
-            this.money.addAll(money);
+        var amount = getFunds() * percent / 100;
+        var history = new MoneyAudit(UUID.randomUUID(), getService(), "rendimentos", OffsetDateTime.now());
+        var money = Stream.generate(() -> new Money(history)).limit(amount).toList();
+        this.money.addAll(money);
     }
 
+    @Override
+    public String toString() {
+        return super.toString() + "InvestmentWallet{" +
+                "investment=" + investment +
+                ", account=" + account +
+                '}';
+    }
 }
---
+++ src/main/java/br/com/dio/repository/AccountRepository.java
@@ -0,0 +1,52 @@
+package br.com.dio.repository;
+
+import br.com.dio.model.AccountWallet;
+
+import javax.security.auth.login.AccountNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccountRepository {
+
+    private final List<AccountWallet> accounts = new ArrayList<>();
+
+    public List<AccountWallet> list(){
+        return this.accounts;
+    }
+
+    public AccountWallet findByPix(final String pix){
+        return accounts.stream()
+                .filter(a -> a.getPix().contains(pix))
+                .findFirst()
+                .orElseThrow(() -> new AccountNotFoundException("A conta com a chave pix '" + pix + "' não existe ou foi encerrada"));
+    }
+
+    public AccountWallet create(final List<String> pix, final long initialFunds){
+        
+        var newAccount = new AccountWallet(initialFunds, pix);
+        accounts.add(newAccount);
+        return newAccount;
+    }
+
+    public void deposit(final String pix, final long fundsAmount){
+        var target = findByPix(pix);
+        target.addMoney(fundsAmount, "depósito");
+    }
+
+    public long withdraw(final String pix, final long amount){
+        var source  = findByPix(pix);
+        checkFundsForTransaction(source, amount);
+        source.reduceMoney(amount);
+        return amount;
+    }
+
+    public void transferMoney(final String sourcePix, final String targetPix, final long amount){
+        var source  = findByPix(sourcePix);
+        checkFundsForTransaction(source, amount);
+        var target = findByPix(targetPix);
+        var message = "pix enviado de '" + sourcePix + "' para '" + targetPix + "'";
+        target.addMoney(source.reduceMoney(amount), source.getService(), message);
+    }
+
+
+}
